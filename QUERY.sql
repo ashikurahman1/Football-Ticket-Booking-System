8826b58379a4758c78ee72ae7dc462852e6cdf8c
ashikurahman1 <ashikurahmantuhin@gmail.com>
2026-06-27 14:43:59 +0600

    Add SQL query 5: List all users with booking information using LEFT JOIN

--- QUERY.sql
+++ QUERY.sql
@@ -135,3 +135,14 @@ INNER JOIN Users u
 ON b.user_id = u.user_id
 INNER JOIN Matches m
 ON b.match_id = m.match_id;
+
+
+-- Query: 5
+SELECT
+    u.user_id,
+    u.full_name,
+    b.booking_id
+FROM Users u
+LEFT JOIN Bookings b
+ON u.user_id = b.user_id
+ORDER BY u.user_id;
