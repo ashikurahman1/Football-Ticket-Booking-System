2cdb13e4749173873fc859c738ab902e90f06265
ashikurahman1 <ashikurahmantuhin@gmail.com>
2026-06-27 14:40:38 +0600

    Create database schema with tables and constraints

--- QUERY.sql
+++ QUERY.sql
@@ -14,11 +14,12 @@ DROP TABLE IF EXISTS Users;
 -- 1. CREATE USERS TABLE
 -- =========================================================================
 CREATE TABLE Users (
-    user_id TYPE,
-    full_name TYPE,
-    email TYPE,
-    role TYPE,
-    phone_number TYPE,
+    user_id INT PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) UNIQUE NOT NULL,
+    role VARCHAR(50) NOT NULL 
+        CHECK (role IN ('Football Fan', 'Ticket Manager')),
+    phone_number VARCHAR(20),
     
     -- Write your constraint to make 'user_id' the Primary Key
     -- Write your constraint to ensure 'email' values are never duplicated
@@ -29,11 +30,13 @@ CREATE TABLE Users (
 -- 2. CREATE MATCHES TABLE
 -- =========================================================================
 CREATE TABLE Matches (
-    match_id TYPE,
-    fixture TYPE,
-    tournament_category TYPE,
-    base_ticket_price TYPE,
-    match_status TYPE,
+    match_id  INT PRIMARY KEY   ,
+    fixture VARCHAR(200) NOT NULL,
+    tournament_category VARCHAR(100) NOT NULL,
+    base_ticket_price DECIMAL(10, 2) NOT NULL
+        CHECK (base_ticket_price >= 0),
+    match_status VARCHAR(50) NOT NULL
+        CHECK (match_status IN ('Available', 'Selling Fast', 'Sold Out')),
     
     -- Write your constraint to make 'match_id' the Primary Key
     -- Write your check constraint to prevent negative ticket prices
@@ -44,12 +47,17 @@ CREATE TABLE Matches (
 -- 3. CREATE BOOKINGS TABLE
 -- =========================================================================
 CREATE TABLE Bookings (
-    booking_id TYPE,
-    user_id TYPE,
-    match_id TYPE,
-    seat_number TYPE,
-    payment_status TYPE,
-    total_cost TYPE,
+    booking_id INT PRIMARY KEY,
+    user_id INT REFERENCES Users(user_id) ON DELETE CASCADE,
+    match_id INT REFERENCES Matches(match_id) ON DELETE CASCADE,
+    seat_number VARCHAR(10) ,
+    payment_status VARCHAR(20) 
+        CHECK (
+            payment_status IN ('Pending', 'Confirmed', 'Cancelled', 'Refunded')
+            OR payment_status IS NULL
+        ),
+    total_cost DECIMAL(10, 2) 
+        CHECK (total_cost >= 0),
     
     -- Write your constraint to make 'booking_id' the Primary Key
     -- Write your Foreign Key constraint linking 'user_id' to the Users table
